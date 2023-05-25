9510310eaec2030d56f167221287672019b21720
iBrainiac <franqhazard@gmail.com>
2023-05-26 01:14:21 +0300

    adding sepolia network

--- contracts/truffle-config.js
+++ contracts/truffle-config.js
@@ -55,6 +55,17 @@ module.exports = {
     // }
   },
 
+  sepolia: {
+    provider: () =>
+      new HDWalletProvider(
+        MNEMONIC,
+        `https://eth-sepolia.g.alchemy.com/v2/${PROJECT_ID}`
+      ),
+    network_id: 11155111, // Goerli's id
+    confirmations: 2, // # of confirmations to wait between deployments. (default: 0)
+    timeoutBlocks: 200, // # of blocks before a deployment times out  (minimum/default: 50)
+    skipDryRun: true, // Skip dry run before migrations? (default: false for public nets )
+  },
   // Set default mocha options here, use special reporters etc.
   mocha: {
     // timeout: 100000
