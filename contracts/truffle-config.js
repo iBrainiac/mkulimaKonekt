7dca22bc8d6c527d8a89203e14421d31795affea
iBrainiac <franqhazard@gmail.com>
2023-05-26 22:54:01 +0300

    testing out sepolia tetnet

--- contracts/truffle-config.js
+++ contracts/truffle-config.js
@@ -17,10 +17,11 @@ module.exports = {
           MNEMONIC,
           `https://eth-sepolia.g.alchemy.com/v2/${PROJECT_ID}`
         ),
-      network_id: 11155111, // Goerli's id
-      confirmations: 2, // # of confirmations to wait between deployments. (default: 0)
-      timeoutBlocks: 200, // # of blocks before a deployment times out  (minimum/default: 50)
-      skipDryRun: true, // Skip dry run before migrations? (default: false for public nets )
+      network_id: 11155111,  // Goerli's id
+      networkCheckTimeout: 100000, 
+      //confirmations: 2, // # of confirmations to wait between deployments. (default: 0)
+      //timeoutBlocks: 200, // # of blocks before a deployment times out  (minimum/default: 50)
+      //skipDryRun: true, // Skip dry run before migrations? (default: false for public nets )
     },
 
     mainnet: {
